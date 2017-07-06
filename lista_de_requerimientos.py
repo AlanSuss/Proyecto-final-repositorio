49f05cd861fe994e0e2145e39212c8ab682766f7
IgnacioTula13 <ignaciotula13@gmail.com>
2017-07-06 15:31:36 -0300

    primer commit

---
+++ lista_de_requerimientos.py
@@ -0,0 +1,24 @@
+"""Proyecto
+
+Requerimiento del Sistema
+
+Registrar datos de preinscripción
+
+1- El sistema solicitará al visitante que llene formulario de preinscripción
+1.1 El sistema solicitará que se ingresen datos particulares.
+1.2 El sistema
+2- El sistema enviará un correo electrónico al solicitante con información del pan de estudio de la carrera y sus incumbencias, datos del instituto y requisitos para la inscripción. 
+
+Generar la inscripción a mesas de examen
+
+1- El sistema solicitará usuario y contraseña
+2- El sistema validará los datos de logueo
+3- El sistema mostrara en pantalla el panel de mesas de examen
+4- El sistema solicitará que se seleccione una materia a inscribirse
+5- El sistema validará datos de Regularidad del alumno (Correlatividades/Asistencias/trabajos prácticos/parciales).
+5.1 El sistema validará Correlatividades.
+5.2 El sistema validará Asistencia de alumno.
+5.3 El sistema validará Trabajos Prácticos. 
+5.4 
+6- El sistema generará la inscripción correspondiente.
+7- El sistema enviara un correo electrónico al estudiante con el comprobante de la inscripción"""
